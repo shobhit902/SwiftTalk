3146cbf0b7dbb3ba60d149406a8843d058553d82
Shobhit Rawat <shobhit1948@gmail.com>
2025-09-19 19:27:33 +0530

    partial frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,15 +1,38 @@
-import { useState } from 'react'
+import React, { Profiler } from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router";
 
-function App() {
+import About from "../src/pages/About.jsx";
+import Contact from "../src/pages/Contact.jsx";
+import Home from "../src/pages/Home.jsx";
 
+import RootLayout from "./layout/RootLayout.jsx";
+import Logout from "./pages/Logout.jsx";
+import Settings from "./pages/Settings.jsx";
+import Profile from "./pages/profile.jsx";
+
+const App = () => {
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="logout" element={<Logout />} />
+        <Route path="settings" element={<Settings />} />
+        <Route path="profile" element={<Profile />} />
+      </Route>
+    )
+  );
   return (
     <>
-    <div>
-     
-      <button className='bg-blue-500 text-white px-4 py-2 rounded'>Button</button>
-    </div>
+      <RouterProvider router={router} />
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ Frontend/src/layout/RootLayout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Outlet } from "react-router";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+const RootLayout = () => {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+export default RootLayout;
